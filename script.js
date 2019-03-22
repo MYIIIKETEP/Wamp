e381efa9ff2d0483a3ff722c363fe11aac2f616b
Denis Jakusjev <Denis.Jakusjev@gmail.com>
2019-03-22 01:09:56 +0100

    wamp

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+var xhr;
+if (window.XMLHttpRequest) xhr = new XMLHttpRequest(); 		// all browsers except IE
+else xhr = new ActiveXObject("Microsoft.XMLHTTP"); 		// for IE
+ 
+xhr.open('GET', 'data.json', false);
+xhr.onreadystatechange = function () {
+	if (xhr.readyState===4 && xhr.status===200) {			
+		var items = JSON.parse(xhr.responseText);
+		var output = "";
+		for (var key in items) {
+			output += "<div id=items class=col-md-3>" + "<h3>"+items[key].name + '</h3>' 
+			+ '<p>' + "Gender: " + items[key].gender + '</p>'+ "</div>";
+		}
+		
+		document.getElementById('flip').innerHTML = output;
+	}
+}
+xhr.send();
+
+$(function(){
+    $("#ClickMe").click(function(){
+        $("#panel").slideToggle("slow");
+        //$("#panel").slideUp("slow"); Css on slide div NO Display:none;
+    });
+  });
