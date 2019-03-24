afa5f8e31742d9edff68d7171043b13c771a8409
Denis Jakusjev <Denis.Jakusjev@gmail.com>
2019-03-24 20:10:03 +0100

    newlines

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ xhr.onreadystatechange = function () {
 		var items = JSON.parse(xhr.responseText);
 		var output = "";
 		for (var key in items) {
-			output += "<div id=items class=col-md-2>" + "<h3>"+items[key].name + '</h3>' 
-			+ '<p>' + "Gender: " + items[key].gender + '</p>'+ "</div>";
+			output += "<div id=items class=col-md-2>" + "<h3 id=Name>"+items[key].name + '</h3>' 
+			+ '<p id=Gender>' + "Gender: " + items[key].gender + '</p>'+ "</div>";
 		}
 		
 		document.getElementById('flip').innerHTML = output;
@@ -24,3 +24,8 @@ $(function(){
         $("#panel").slideToggle("fast");
     });
   });
+
+  //Lægga till knappen "Add to Cart" på den ta this div #name och #gender och senare göra ett append
+  //efter (.table thead) med tanke på att namn och pris i inlagda i thead, vi lääger bara in <th> + 
+  //<td> objName </td> och <td>ObjGender</th> och sen vi kan ta hela <.table HTML kod och lägge in den på 
+  //confirm sidan
